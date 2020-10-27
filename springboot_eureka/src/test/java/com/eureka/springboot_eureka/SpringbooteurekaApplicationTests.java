594dfdc1133eaad221da4fc8c7ec6134f0804810
saravana <saravanakmr672@gmail.com>
2020-10-27 15:55:08 +0530

    a

---
+++ springboot_eureka/src/test/java/com/eureka/springboot_eureka/SpringbooteurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.springboot_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbooteurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
